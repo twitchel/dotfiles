f97278e9b1ce80e7d60e3f37b9ef3de9120d6996
Daniel K. Jones <me@danielkjones.com>
2024-04-08 10:52:13 +1000

    Initial commit, added bootstrap and brewfile

---
+++ bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+# Install zsh, ohmyzsh powerlevel10k, homebrew
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".idea/" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms ./home/ ~;
+#	source ~/.zshrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
