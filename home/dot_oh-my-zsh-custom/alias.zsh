aee42f1e5dff0c5f3230f7aa6cdd238ac4e50b37
Daniel K. Jones <daniel@karta.com.au>
2024-05-29 09:10:49 +1000

    Add aliases for ls and cd, with original backups

--- home/dot_oh-my-zsh-custom/alias.zsh
+++ home/dot_oh-my-zsh-custom/alias.zsh
@@ -1,4 +1,10 @@
+# Work Dirs
 alias dgh="cd ~/dev/src/github.com/"
 alias tdd="cd ~/dev/src/github.com/twitchel/"
+alias wdd="cd ~/dev/src/github.com/545490/"
 
+# Application replacements
+alias _ls="ls"
 alias ls="eza --color=always --long --git --icons=always"
+alias _cd="cd"
+alias cd="z"
