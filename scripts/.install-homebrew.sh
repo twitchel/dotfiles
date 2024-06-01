f25222c87a4081a17f54d0bf1f2b0c9aebdc7833
Daniel K. Jones <me@danielkjones.com>
2024-06-01 15:46:21 +1000

    Add linux bootstrap and brew installer

--- scripts/.install-homebrew.sh
+++
@@ -1,6 +0,0 @@
-printf "\e[1;33mBootstrap\e[0m\n"
-
-	type brew >/dev/null 2>&1 && printf "Brew already installed" && exit
-
-printf "Installing Brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
