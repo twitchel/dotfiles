6676363ea0c5be641c0bf568dacebd8d2396bdfd
Daniel K. Jones <daniel@karta.com.au>
2024-06-03 09:15:21 +1000

    Update setting shell script
    - Adds current zsh path to /etc/shells
    - Sets current shell to zsh

--- home/.chezmoiscripts/run_once_after_90_default_shell.sh
+++ home/.chezmoiscripts/run_once_after_90_default_shell.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-chsh -s "$(which zsh)"
+ZSH_PATH=$(which zsh)
+
+# Add zsh to list of valid shells in /etc/shells
+echo $ZSH_PATH | sudo tee -a /etc/shells
+
+# Set default shell to zsh
+chsh -s $ZSH_PATH
