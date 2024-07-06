e501a305d291c520d252a58d38fbe607ab567c47
Daniel K. Jones <contact@danieljones.net>
2024-07-06 23:30:08 +1000

    Finish setup of nvim as per josean martin

--- home/dot_config/nvim/lua/daniel/lazy.lua
+++ home/dot_config/nvim/lua/daniel/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("daniel.plugins")
+require("lazy").setup({ { import = "daniel.plugins" }, { import = "daniel.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  }
+})
