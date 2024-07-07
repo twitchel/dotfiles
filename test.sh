705841bf40b5e1cd2ce51515fd4b0ca0e62de104
Daniel Jones <contact@danieljones.net>
2024-07-08 01:04:24 +1000

    Only source zshrc if the ZSH env isnt set

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ echo "Prepping Tests"
 echo "Running as user: $(whoami)"
 echo "Running in shell: $(echo $0)"
 echo "Running in dir: $(pwd)"
-echo "Sourcing zshrc"
-source $HOME/.zshrc
+
+if [[ -z "${ZSH}" ]]; then
+  echo "Sourcing zshrc"
+  source $HOME/.zshrc
+fi;
 
 for FILE in tests/*; do
   sh $FILE;
