44c643ece7e6250587bc71240027ddb90f978923
Daniel Jones <contact@danieljones.net>
2024-07-08 00:39:16 +1000

    Source zshrc in tests file

--- test.sh
+++ test.sh
@@ -2,6 +2,13 @@
 
 exitCode=0
 
+echo "Prepping Tests"
+echo "Running as user: $(whoami)"
+echo "Running in shell: $(echo $0)"
+echo "Running in dir: $(pwd)"
+echo "Sourcing zshrc"
+source $HOME/.zshrc
+
 for FILE in tests/*; do
   sh $FILE;
   if [[ $? -ne 0 ]]; then
