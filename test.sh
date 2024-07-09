031ff46286ad41cbbf5c741583d298bee554f09a
Daniel Jones <contact@danieljones.net>
2024-07-09 18:34:23 +1000

    Add fix for error logging in CI test suite

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+if [[ -z ${ZSH_SOURCED} ]]; then
+  source ~/.zshrc
+fi
+
+if [ "${TERM:-}" = "" ]; then
+  echo "Setting TERM to dumb" # makes tput happy in CI envs
+  TERM="dumb"
+fi
+
+# Run the tests
+zunit --output-text
