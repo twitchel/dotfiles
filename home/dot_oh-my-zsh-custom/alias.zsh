91e9ed78f36753ab8347976323d120e638aef916
Daniel Jones <contact@danieljones.net>
2024-07-11 22:42:58 +1000

    Simplify zsh config

--- home/dot_oh-my-zsh-custom/alias.zsh
+++ home/dot_oh-my-zsh-custom/alias.zsh
@@ -1,12 +1,1 @@
-# Work Dirs
-alias dgh="cd ~/dev/src/github.com/"
-alias tdd="cd ~/dev/src/github.com/twitchel/"
-alias wdd="cd ~/dev/src/github.com/545490/"
 
-# Application replacements
-alias og-ls="ls"
-alias ls="eza --color=always --long --git --icons=always"
-alias og-cd="cd"
-alias cd="z"
-
-alias v="nvim" # neovim
