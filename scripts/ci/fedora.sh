93dfb5d15980679280f49edb32f467c97037cfaf
Daniel Jones <contact@danieljones.net>
2026-02-09 08:40:07 +1100

    test: re-enable matrix tests

--- scripts/ci/fedora.sh
+++ scripts/ci/fedora.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Install Chezmoi and apply the dotfiles
+mkdir -p $HOME/.local/bin
+export PATH="$HOME/.local/bin:$PATH"
 sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 
 # Apply the dotfiles
-chezmoi init --apply --source "$(pwd)" --no-tty
+chezmoi init --source "$(pwd)" --no-tty          
+chezmoi apply --source "$(pwd)" --no-tty
